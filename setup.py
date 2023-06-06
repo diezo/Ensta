ee23fed7e6de6eb7ac63a537e2fef143f33b1401
Deepak Soni <inosmoon@gmail.com>
2023-06-06 21:49:53 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 setup(
     name="ensta",
     packages=["ensta"],
-    version="0.4",
+    version="0.5",
     license="MIT",
     description="Simple & Up-to-date Instagram API",
     long_description=long_description,
