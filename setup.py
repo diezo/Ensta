7d3206e78f959f87f4925ddc86170e9e8d31d970
Deepak Soni <inosmoon@gmail.com>
2023-06-09 14:59:57 +0530

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="ensta",
-    packages=["ensta", "ensta.lib", "ensta.containers", "ensta.responses"],
+    packages=["ensta", "ensta.lib", "ensta.containers"],
     version="1.4",
     license="MIT",
     description="Simple & Up-to-date Instagram API",
