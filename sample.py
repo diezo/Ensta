ae7be4572177ff729739c9d52213333a78385c93
Deepak Soni <inosmoon@gmail.com>
2023-06-09 23:07:09 +0530

    added 'change account privacy' functions

--- sample.py
+++
@@ -1,9 +0,0 @@
-from ensta import Guest, Host
-
-sessionid = "<Replace with your session id>"
-
-guest = Guest()  # Doesn't require login
-host = Host(sessionid)  # Requires login through sessionid
-
-print(guest.profile("cristiano"))  # Prints user's profile information
-print(host.follow("cristiano"))  # Follows the username or uid mentioned
