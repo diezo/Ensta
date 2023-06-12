bf499ce88c42889a83e9d93af67734b2f2226dae
Deepak Soni <inosmoon@gmail.com>
2023-06-12 21:34:48 +0530

    changed version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 from pathlib import Path
 
-long_description = (Path(__file__).parent / "README.md").read_text()
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="ensta",
     packages=["ensta", "ensta.lib", "ensta.containers"],
-    version="1.4",
+    version="1.5",
     license="MIT",
     description="Simple & Up-to-date Instagram API",
     long_description=long_description,
@@ -14,11 +14,11 @@ setup(
     author="Deepak Soni",
     author_email="lonelycube@proton.me",
     url="https://github.com/diezo/ensta",
-    download_url="https://github.com/diezo/ensta/archive/refs/tags/v1.4.tar.gz",
+    download_url="https://github.com/diezo/ensta/archive/refs/tags/v1.5.tar.gz",
     keywords=["instagram", "web", "private", "api", "scraper", "easy", "download", "upload"],
     install_requires=["requests"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
