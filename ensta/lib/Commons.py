a8abca0c810e15a991905ca42ae69c8e1dadc14b
Deepak Soni <inosmoon@gmail.com>
2023-06-13 19:42:06 +0530

    optimizations

--- ensta/lib/Commons.py
+++ ensta/lib/Commons.py
@@ -24,7 +24,24 @@ def update_session(self) -> None:
 
 
 def update_homepage_source(self) -> None:
-    temp_homepage_source = requests.get("https://www.instagram.com").text.strip()
+    request_headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-ch-prefers-color-scheme": "dark",
+        "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+        "sec-ch-ua-full-version-list": "\"Not.A/Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"114.0.5735.110\", \"Google Chrome\";v=\"114.0.5735.110\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-ch-ua-platform-version": "\"15.0.0\"",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "viewport-width": "1475",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
+    temp_homepage_source = requests.get("https://www.instagram.com/", headers=request_headers).text.strip()
 
     if temp_homepage_source != "":
         self.homepage_source = temp_homepage_source
