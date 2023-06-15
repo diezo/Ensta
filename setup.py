0c879f2c57976b21b45aaf7f6921ff426605a4da
Deepak Soni <inosmoon@gmail.com>
2023-06-15 20:04:06 +0530

    updated pip version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf
 setup(
     name="ensta",
     packages=["ensta", "ensta.lib", "ensta.containers"],
-    version="2.4",
+    version="2.5",
     license="MIT",
     description="🔥 Fastest & Simplest Python Package For Instagram Automation",
     long_description=long_description,
@@ -14,7 +14,7 @@ setup(
     author="Deepak Soni",
     author_email="lonelycube@proton.me",
     url="https://github.com/diezo/ensta",
-    download_url="https://github.com/diezo/ensta/archive/refs/tags/v2.4.tar.gz",
+    download_url="https://github.com/diezo/ensta/archive/refs/tags/v2.5.tar.gz",
     keywords=["instagram-client", "instagram", "api-wrapper", "instagram-scraper", "instagram-api", "instagram-sdk", "instagram-photos", "instagram-api-python", "instabot", "instagram-stories", "instagram-bot", "instapy", "instagram-downloader", "instagram-account", "instagram-crawler", "instagram-private-api", "igtv", "instagram-automation", "reels", "instagram-feed"],
     install_requires=["requests", "selenium"],
     classifiers=[
