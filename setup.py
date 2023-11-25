1acc1cfae029d81bc1c942314d9472eaf94d3b97
Deepak Soni <sonniiii@outlook.com>
2023-11-25 19:26:49 +0530

    code optimizations

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from pathlib import Path
 
-version = "5.0.3"
+version = "5.0.3.1"
 long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
