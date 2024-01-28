84de19b0f01230a27fe5ade9ad1d4366c79af4a2
Deepak Soni <sonniiii@outlook.com>
2024-01-28 12:42:57 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from pathlib import Path
 
-version = "5.0.9"
+version = "5.1.0"
 long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
